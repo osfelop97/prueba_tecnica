334650942f494b09d67a3320dbd9497edd2c5004
Oscar Felipe <oscarfelipe978@hotmail.com>
2025-08-24 02:22:37 -0500

    actualizacion y subida de script

---
+++ script_tablas.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE `prueba_tecnica` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
+USE `prueba_tecnica`;
+
+CREATE TABLE `productos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` text,
+  `price` decimal(10,2) DEFAULT NULL,
+  `category` text,
+  `description` text,
+  `fecha_insercion` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `estado` (
+  `idEstado` int NOT NULL AUTO_INCREMENT,
+  `Tarea` text,
+  `Estado` text,
+  PRIMARY KEY (`idEstado`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+INSERT IGNORE INTO productos (id) VALUES (1);
